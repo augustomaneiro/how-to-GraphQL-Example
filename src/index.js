10178706150c6129507f53cf647816aa021f0eac
Augusto Maneiro <augustom@nimble.la>
2019-06-27 14:53:14 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    }
+  },
+  Mutation: {
+    // 2
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
+    },
+    updateLink: (root, args, context) => {
+      });
+    },
+    deleteLink: (parent, args) => {
+      links.forEach((link, index) => {
+        if (link.id === args.id) {
+          links.splice(index, 1);
+          return link;
+        }
+      });
+    }
+  }
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: { prisma }
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
